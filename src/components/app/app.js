84911d2cbbfcb013448228f5627a250d7cacabdc
Tatiana Tikhonova <tanianova@yandex.ru>
2022-01-06 21:22:19 +0300

    added search, filter

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,6 +17,8 @@ class App extends Component {
         { id: 2, name: "Alex M.", salary: 1000, increase: false, rise: false },
         { id: 3, name: "Carl W.", salary: 4500, increase: false, rise: false },
       ],
+      term: "",
+      filter: "all",
     };
     this.newId = 4;
   }
@@ -52,8 +54,8 @@ class App extends Component {
     });
   };
 
-  onToggleProp=(id,prop)=>{
-          this.setState(({ data }) => ({
+  onToggleProp = (id, prop) => {
+    this.setState(({ data }) => ({
       data: data.map((item) => {
         if (item.id === id) {
           return { ...item, [prop]: !item[prop] };
@@ -61,48 +63,78 @@ class App extends Component {
         return item;
       }),
     }));
-  }
+  };
 
-//   onToggleIncrease = (id) => {
-    //first method выдергиваем рандомный объект из рандомного места
-    // this.setState(({ data }) => {
-    //   const index = data.findIndex((elem) => elem.id === id);
-    //   const old = data[index];
-    //   const newItem = { ...old, increase: !old.increase };
-    //   const newArr = [
-    //     ...data.slice(0, index),
-    //     newItem,
-    //     ...data.slice(index + 1),
-    //   ];
-    //   return {
-    //     data: newArr,
-    //   };
-    // });
+  //   onToggleIncrease = (id) => {
+  //first method выдергиваем рандомный объект из рандомного места
+  // this.setState(({ data }) => {
+  //   const index = data.findIndex((elem) => elem.id === id);
+  //   const old = data[index];
+  //   const newItem = { ...old, increase: !old.increase };
+  //   const newArr = [
+  //     ...data.slice(0, index),
+  //     newItem,
+  //     ...data.slice(index + 1),
+  //   ];
+  //   return {
+  //     data: newArr,
+  //   };
+  // });
 
-    //второй метод
-//     this.setState(({ data }) => ({
-//       data: data.map((item) => {
-//         if (item.id === id) {
-//           return { ...item, increase: !item.increase };
-//         }
-//         return item;
-//       }),
-//     }));
-//   };
+  //второй метод
+  //     this.setState(({ data }) => ({
+  //       data: data.map((item) => {
+  //         if (item.id === id) {
+  //           return { ...item, increase: !item.increase };
+  //         }
+  //         return item;
+  //       }),
+  //     }));
+  //   };
+
+  searchEmp = (items, term) => {
+    if (term.length === 0) {
+      return items;
+    }
+    return items.filter((item) => {
+      return item.name.indexOf(term) > -1;
+    });
+  };
+
+  onUpdateSearch = (term) => {
+    this.setState({ term });
+  };
+
+  filterPost = (items, filter) => {
+    switch (filter) {
+      case "rise":
+        return items.filter((item) => item.rise);
+      case "moreThan1000":
+        return items.filter((item) => item.salary > 1000);
+      default:
+        return items;
+    }
+  };
+
+  onFilterSelect = (filter) => {
+    this.setState({ filter });
+  };
 
   render() {
+    const { data, term, filter } = this.state;
     const employees = this.state.data.length;
     const increased = this.state.data.filter((item) => item.increase).length;
+    const visibleData = this.filterPost(this.searchEmp(data, term), filter);
 
     return (
       <div className="app">
         <AppInfo employees={employees} increased={increased} />
         <div className="search-panel">
-          <SearchPanel />
-          <AppFilter />
+          <SearchPanel onUpdateSearch={this.onUpdateSearch} />
+          <AppFilter filter={filter} onFilterSelect={this.onFilterSelect} />
         </div>
         <EmployeesList
-          data={this.state.data}
+          data={visibleData}
           onDelete={this.deleteItem}
           onToggleProp={this.onToggleProp}
         />
